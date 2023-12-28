25be8b7e1ffa8a3e9bdbc904f887511c8572ce2f
tzptzptzptzptzp <git@tzp.pink>
2023-12-29 02:25:26 +0900

    ✨ Formの雛形を作成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,9 @@
+import { SignUpContainer } from "@/components/organisms/SignUpContainer";
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24"></main>
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <SignUpContainer />
+    </main>
   );
 }
