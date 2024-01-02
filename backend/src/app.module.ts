f61b40f4ccacba29f7784b208de242136b2dc126
tzptzptzptzptzp <git@tzp.pink>
2024-01-02 23:40:42 +0900

    🔧 FirebaseModuleを生成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, FirebaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
