89a5fe7fc613809762ae8d21e421faf99e2a5384
tzptzptzptzptzp <git@tzp.pink>
2024-01-08 22:39:15 +0900

    ✨ CORS設定を追加

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,14 @@ const PORT = 5555;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS設定
+  const allowedOrigins: Array<string> = ['http://localhost:3000'];
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+
   await app.listen(PORT);
 }
 bootstrap();
