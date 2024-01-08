2e0fa524ddf8933c10f88bde5344bb154f54cc08
tzptzptzptzptzp <git@tzp.pink>
2024-01-08 23:04:34 +0900

    ✨ FirebaseModuleをインポート

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { FirebaseModule } from 'src/firebase/firebase.module';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  imports: [FirebaseModule],
+  providers: [UserService],
 })
 export class UserModule {}
