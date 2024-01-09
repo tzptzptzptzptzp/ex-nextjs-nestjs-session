aef312d010bddf840cad109d1c55dcf7dc93970f
tzptzptzptzptzp <git@tzp.pink>
2024-01-09 23:20:44 +0900

    🔧 セッションControllerを作成

--- backend/src/session/session.module.ts
+++ backend/src/session/session.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { SessionController } from './session.controller';
 
-@Module({})
+@Module({
+  controllers: [SessionController]
+})
 export class SessionModule {}
