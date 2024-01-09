3c6a66fe09d8466aab28a8234125b5d9c9429ae9
tzptzptzptzptzp <git@tzp.pink>
2024-01-09 22:44:19 +0900

    ✨ ユーザーデータ作成APIのコントローラーを定義

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // ユーザーデータ作成
+  @Post('/create')
+  async createUserData(
+    @Body('uid') uid: string,
+    @Body('email') email: string,
+  ): Promise<string> {
+    return await this.userService.createUserData(uid, email);
+  }
+}
