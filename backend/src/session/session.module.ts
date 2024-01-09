4e66fc147b63628fda1fe9302ff320113dad48ed
tzptzptzptzptzp <git@tzp.pink>
2024-01-09 23:22:18 +0900

    🔧 セッションServiceを作成

--- backend/src/session/session.module.ts
+++ backend/src/session/session.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SessionController } from './session.controller';
+import { SessionService } from './session.service';
 
 @Module({
-  controllers: [SessionController]
+  controllers: [SessionController],
+  providers: [SessionService]
 })
 export class SessionModule {}
