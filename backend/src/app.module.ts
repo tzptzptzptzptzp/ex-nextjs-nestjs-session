ec980425b424e216eaca000f52d3f5874854065c
tzptzptzptzptzp <git@tzp.pink>
2024-01-09 23:19:20 +0900

    🔧 セッションModuleを作成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { FirebaseModule } from './firebase/firebase.module';
+import { SessionModule } from './session/session.module';
 
 @Module({
-  imports: [UserModule, FirebaseModule],
+  imports: [UserModule, FirebaseModule, SessionModule],
   controllers: [AppController],
   providers: [AppService],
 })
