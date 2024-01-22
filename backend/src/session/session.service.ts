2db853a6bfcd9f42601dea6424ff5cee56d57f48
tzptzptzptzptzp <git@tzp.pink>
2024-01-22 21:19:23 +0900

    ✨ トークンを生成する処理を実装

--- backend/src/session/session.service.ts
+++ backend/src/session/session.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class SessionService {}
+export class SessionService {
+  constructor(private jwtService: JwtService) {}
+
+  /**
+   * トークン生成
+   * @param uid
+   */
+  async createToken(uid: string): Promise<string> {
+    const payload = { uid };
+    return this.jwtService.sign(payload);
+  }
+}
