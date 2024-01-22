ac958d99354d23fa6a3f4de6ec51d6849e978cf8
tzptzptzptzptzp <git@tzp.pink>
2024-01-22 21:19:55 +0900

    ✨ SessionModuleを変更

--- backend/src/session/session.module.ts
+++ backend/src/session/session.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SessionController } from './session.controller';
 import { SessionService } from './session.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [SessionController],
-  providers: [SessionService]
+  imports: [
+    JwtModule.register({
+      secret: 'secret-key',
+      signOptions: { expiresIn: '1w' },
+    }),
+  ],
+  providers: [SessionService],
 })
 export class SessionModule {}
