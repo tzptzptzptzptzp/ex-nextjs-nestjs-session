485251053388ddd9a49dd453899b030cdc0aaef3
tzptzptzptzptzp <git@tzp.pink>
2024-01-25 23:05:06 +0900

    ✨ トークン検証APIを実装

--- backend/src/session/session.service.ts
+++ backend/src/session/session.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
+interface TokenPayload {
+  uid: string;
+}
+
 @Injectable()
 export class SessionService {
   constructor(private jwtService: JwtService) {}
@@ -12,5 +16,17 @@ export class SessionService {
   async createToken(uid: string): Promise<string> {
     const payload = { uid };
     return this.jwtService.sign(payload);
+  }
+
+  /**
+   * トークン検証
+   * @param token
+   */
+  async verifyToken(token: string): Promise<TokenPayload> {
+    try {
+      return this.jwtService.verify(token);
+    } catch (err) {
+      throw new Error('Invalid token');
+    }
   }
 }
