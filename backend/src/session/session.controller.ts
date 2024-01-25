25eaacf8016fd5b279f138d174f576102e1e3448
tzptzptzptzptzp <git@tzp.pink>
2024-01-25 23:05:37 +0900

    ✨ APIのルーティングを設定

--- backend/src/session/session.controller.ts
+++ backend/src/session/session.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Headers,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { SessionService } from './session.service';
 
 @Controller('session')
-export class SessionController {}
+export class SessionController {
+  constructor(private sessionService: SessionService) {}
+
+  @Get('/verify')
+  async verifyToken(@Headers('authorization') token: string) {
+    if (!token) {
+      throw new HttpException('No token provided', HttpStatus.BAD_REQUEST);
+    }
+
+    try {
+      const decoded = await this.sessionService.verifyToken(token);
+      return { vaild: true, uid: decoded.uid };
+    } catch (err) {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
+  }
+}
