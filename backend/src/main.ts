06aa0661c086ec5521eecab89e68a8bd2f77460c
tzptzptzptzptzp <git@tzp.pink>
2024-01-29 01:21:54 +0900

    ⚡️ cookieParserを使用するように変更

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 const PORT = 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   // CORS設定
   const allowedOrigins: Array<string> = ['http://localhost:3000'];
