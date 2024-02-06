ee87915fad7f3e4e1d50ad7585e3bb7dcfc8b9f2
tzptzptzptzptzp <git@tzp.pink>
2024-02-06 23:47:04 +0900

    ⚡️ Templateでトークンの検証を行うように変更

--- frontend/src/app/template.tsx
+++ frontend/src/app/template.tsx
@@ -1,6 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
+
+import useSWR from "swr";
+
 import { Header } from "@/components/organisms/Header";
 
+const API_URL = "http://localhost:8080/session/verify";
+
+const fetcher = (url: string) =>
+  fetch(url, {
+    method: "GET",
+    credentials: "include",
+  }).then((res) => res.json());
+
 export default function Template({ children }: { children: React.ReactNode }) {
+  const [session, setSession] = useState(null);
+
+  const { data, error, isLoading } = useSWR(API_URL, fetcher);
+
+  useEffect(() => {
+    if (data && data.vaild) {
+      setSession(data);
+    } else {
+      setSession(null);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
+
+  if (isLoading) return;
   return (
     <>
       <Header />
