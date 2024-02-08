57a238c21bdec6890badaa481e9ab63e63fafd3b
tzptzptzptzptzp <git@tzp.pink>
2024-02-08 23:51:02 +0900

    ✨ Reducerを作成

---
+++ frontend/src/reducks/users/reducers.ts
@@ -0,0 +1,7 @@
+import { Action } from "redux";
+import { initialUsersState } from "./initializes";
+
+export const UsersReducer = (state = initialUsersState, action: Action) => {
+  switch (action.type) {
+  }
+};
