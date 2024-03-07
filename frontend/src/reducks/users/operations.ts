68dc27a87a5ca0d310bb8364681a7da65d51bc04
tzptzptzptzptzp <git@tzp.pink>
2024-03-07 22:50:53 +0900

    ✏️ Prefixを更新

--- frontend/src/reducks/users/operations.ts
+++ frontend/src/reducks/users/operations.ts
@@ -5,7 +5,7 @@ import { config } from "@/config/api.config";
 import { SignUp } from "@/utils/SignUp.util";
 
 export const SignUpVerify = createAsyncThunk(
-  "user/signUp",
+  "user/signUpVerify",
   async (req: UserType) => {
     await SignUp(req);
     const res = await axios.get(`${config.API_BASE_URL}session/verify`, {
