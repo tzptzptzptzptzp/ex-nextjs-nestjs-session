507fdcb0519fb49179b5554173234e5ddeade304
tzptzptzptzptzp <git@tzp.pink>
2024-03-08 02:00:04 +0900

    ✨ AuthProviderを作成

--- frontend/src/app/template.tsx
+++ frontend/src/app/template.tsx
@@ -1,11 +1,14 @@
 import { StateProvider } from "@/provider/State.provider";
 import { Header } from "@/components/organisms/Header";
+import { AuthProvider } from "@/provider/Auth.provider";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <StateProvider>
-      <Header />
-      {children}
+      <AuthProvider>
+        <Header />
+        {children}
+      </AuthProvider>
     </StateProvider>
   );
 }
---
+++ frontend/src/provider/Auth.provider.tsx
@@ -0,0 +1,3 @@
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
