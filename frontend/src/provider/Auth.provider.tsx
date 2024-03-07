c78a3e26ae640cfb353aa04a601fd15fdc83ba7b
tzptzptzptzptzp <git@tzp.pink>
2024-03-08 02:00:56 +0900

    ✨ 認証状態取得中に非表示にする処理を追加

--- frontend/src/provider/Auth.provider.tsx
+++ frontend/src/provider/Auth.provider.tsx
@@ -1,3 +1,21 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useVerifySessionDispatch } from "@/reducks/users/dispatches";
+import { userSelector } from "@/reducks/users/selecors";
+
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+  const [isLoading, setIsLoading] = useState(true);
+  const { loading } = userSelector();
+  useEffect(() => {
+    useVerifySessionDispatch();
+  }, []);
+  useEffect(() => {
+    if (loading) {
+      setIsLoading(false);
+    }
+  }, [loading]);
+
+  if (loading) return <div>...loading</div>;
+
+  if (!isLoading) return <>{children}</>;
 };
