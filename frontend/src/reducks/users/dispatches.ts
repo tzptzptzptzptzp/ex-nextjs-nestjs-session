70b9beae27a4876a4451a001c8043efc0dc028ba
tzptzptzptzptzp <git@tzp.pink>
2024-03-15 22:41:49 +0900

    ✨ サインイン用Dispatch関数を実装

--- frontend/src/reducks/users/dispatches.ts
+++ frontend/src/reducks/users/dispatches.ts
@@ -1,9 +1,13 @@
 import { store } from "../store";
-import { SignUpVerify, VerifySession } from "./operations";
-import { SignUpType } from "./types";
+import { SignInVerify, SignUpVerify, VerifySession } from "./operations";
+import { SignInType, SignUpType } from "./types";
 
 export const useSignUpVerifyDispatch = (req: SignUpType) => {
   store.dispatch(SignUpVerify(req));
+};
+
+export const useSignInVerifyDispatch = (req: SignInType) => {
+  store.dispatch(SignInVerify(req));
 };
 
 export const useVerifySessionDispatch = () => {
