62ab701f4d16bd29984deaa0d10c7c8123de4c45
tzptzptzptzptzp <git@tzp.pink>
2024-03-18 00:51:38 +0900

    ⚡️ 認証していない時の処理をコンポーネントに切り出し

--- frontend/src/app/mypage/page.tsx
+++ frontend/src/app/mypage/page.tsx
@@ -1,17 +1,16 @@
 "use client";
-import { useRouter } from "next/navigation";
 import { userSelector } from "@/reducks/users/selecors";
 import { ProfileCard } from "@/components/molecules/ProfileCard/ProfileCard.molecule";
+import { SessionFrame } from "@/components/templates/sessionFrame.template";
 
 export default function Mypage() {
-  const router = useRouter();
   const { user } = userSelector();
-
-  if (!user) router.push("/signin");
-  else
+  if (user)
     return (
-      <main className="flex flex-col items-center justify-between min-h-screen p-36">
-        <ProfileCard email={user?.email} username={user?.username} />
-      </main>
+      <SessionFrame>
+        <main className="flex flex-col items-center justify-between min-h-screen p-36">
+          <ProfileCard email={user?.email} username={user?.username} />
+        </main>
+      </SessionFrame>
     );
 }
