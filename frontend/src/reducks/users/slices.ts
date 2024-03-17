b03e97b8edf9b9af97a081183222e5ae71bd027f
tzptzptzptzptzp <git@tzp.pink>
2024-03-17 23:51:39 +0900

    ✨ Reducerを適用する処理を追加

--- frontend/src/reducks/users/slices.ts
+++ frontend/src/reducks/users/slices.ts
@@ -1,12 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 import { initialUserState } from "./initializes";
 import { userReducers } from "./reducers";
 
 const userSlice = createSlice({
   name: "user",
   initialState: initialUserState,
-  reducers: {},
+  reducers: userReducers.reducers,
   extraReducers: userReducers.extraReducers,
 });
 
