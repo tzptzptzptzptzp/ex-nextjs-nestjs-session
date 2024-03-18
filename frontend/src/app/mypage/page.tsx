3deead546ae1c5e09ce27bc1f7053f3130520839
tzptzptzptzptzp <git@tzp.pink>
2024-03-19 00:05:30 +0900

    ⚡️ 非認証時にMypageからSignInへリダイレクトさせるように処理を追加

--- frontend/src/app/mypage/page.tsx
+++ frontend/src/app/mypage/page.tsx
@@ -1,11 +1,23 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { userSelector } from "@/reducks/users/selecors";
 import { ProfileCard } from "@/components/molecules/ProfileCard/ProfileCard.molecule";
 import { SessionFrame } from "@/components/templates/sessionFrame.template";
 
 export default function Mypage() {
-  const { user } = userSelector();
-  if (user)
+  const router = useRouter();
+  const { user, loading } = userSelector();
+
+  if (loading)
+    return (
+      <main className="flex flex-col items-center justify-between min-h-screen p-36">
+        <div>Loading...</div>
+      </main>
+    );
+
+  if (!user) router.push("/signin");
+
+  if (user && !loading)
     return (
       <SessionFrame>
         <main className="flex flex-col items-center justify-between min-h-screen p-36">
